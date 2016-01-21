7869692c78dc5471c3869d1f2d039e424090aa70
mclove02 <jangonion@gmail.com>
2016-01-21 10:43:21 -0500

    third

--- sketch.js
+++ sketch.js
@@ -26,23 +26,23 @@ function setup() {
 }
 
 function draw(){
- col.r = random(0, 255);
- col.g = random(0, 255);
- col.b = random(0, 255);
- noFill();
-// fill(col.r, col.g, col.b);
+ col.r = random(200, 255);
+ col.g = random(100, 255);
+ col.b = random(200, 255);
+// noFill();
+ fill(col.r, col.g, col.b);
  var level = amplitude.getLevel();
  var size = map(level, 0, 1, 0, 1000);
- 
+ noStroke();
   // stroke(col.r, 100, 100);
-  background(size,size,120);
-  // ellipse(width/2, height/4, size/2, size/2);
-   ellipse(width/4, height/2, size, size);
+//  background(size,200);
+   ellipse(width/2, height/4, size/2, size/2);
+//   ellipse(width/4, height/2, size, size);
   
-  // if(size > 200) {
-  //   background(col.r,100, 50);
-  // } else {
-  //   background(255,10);
-  // }
+   if(size > 200) {
+     background(col.r,100, 50);
+   } else {
+     background(255,10);
+   }
 };
 
